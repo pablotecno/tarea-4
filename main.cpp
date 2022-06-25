f6bc86f271db552be1c172bf215f5962d57c7633
pablotecno <104280118+pablotecno@users.noreply.github.com>
2022-06-25 18:50:16 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+/*
+  Tecnologo en Informatica - LATU
+  Principios de Programacion - 2022
+*/
+
+#include <stdio.h>
+#include "tarea.h"
+
+void leerMatriz(int m[20][20]){
+  for (int i = 0; i < 20*20; i++)
+    m[0][i] = 1;
+}
+
+int main() {
+  int matriz[20][20];
+  int n;
+  scanf("%d", &n);
+  
+  while(n != -1){
+    switch(n){
+      case 1 :
+        descrifrar();
+        break;
+      case 2 :
+        leerMatriz(matriz);
+        printf("%d", maxProd3(matriz));
+        break;
+    }
+    scanf("%d", &n);
+  }
+  
+  return 0;
+}
