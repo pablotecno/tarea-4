7d4fe51ec544622c9c758b1546a622912349c96c
pablotecno <104280118+pablotecno@users.noreply.github.com>
2022-06-25 19:00:13 -0300

    Update tarea.cpp

--- tarea.cpp
+++ tarea.cpp
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include "tarea.h"
 
-void descrifrar(){}
+void descrifrar(){
+  char a[500];
+  char b[500];
+  scanf("%s\n", a);
+  scanf("%s", b);
+  printf("%s\n%s", a, b);
+}
 
 int maxProd3(const int arr[20][20]){
   return 0;
